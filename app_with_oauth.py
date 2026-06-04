a9273ea0b18ecf1d2647e06f73bba41f9ebc25a8
adithyaa16 <150831290+adithyaa16@users.noreply.github.com>
2026-06-04 14:03:19 +0530

    Update app_with_oauth.py

--- app_with_oauth.py
+++ app_with_oauth.py
@@ -220,7 +220,7 @@ if not st.session_state.logged_in:
         result = oauth2.authorize_button(
             name="Continue with Google",
             icon="https://www.google.com/favicon.ico",
-            redirect_uri="http://localhost:8501/",
+            redirect_uri="https://saas-business-analytics-gfokedzjs3iwrmkpywqo5z.streamlit.app/",
             scope="openid email profile",
             use_container_width=True,
             pkce="S256",
